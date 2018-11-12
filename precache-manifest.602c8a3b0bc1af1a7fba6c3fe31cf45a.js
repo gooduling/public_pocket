8d7464dec68158f5505184bfeebbf4e7f35ad3c9
Andrii Ulianenko <andrii.ulianenko@soulplatform.com>
2018-11-12 16:31:08 +0100

    Updates

---
+++ precache-manifest.602c8a3b0bc1af1a7fba6c3fe31cf45a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e65bb9b53ae4ca87116b",
+    "url": "/policymonitor/static/js/runtime~main.e65bb9b5.js"
+  },
+  {
+    "revision": "e43c03b08454bc4fa466",
+    "url": "/policymonitor/static/js/main.e43c03b0.chunk.js"
+  },
+  {
+    "revision": "fd84c7365a7066c43519",
+    "url": "/policymonitor/static/js/1.fd84c736.chunk.js"
+  },
+  {
+    "revision": "e43c03b08454bc4fa466",
+    "url": "/policymonitor/static/css/main.b4abf364.chunk.css"
+  },
+  {
+    "revision": "e333b22f7940931e5b4356d2e96a0cca",
+    "url": "/policymonitor/index.html"
+  }
+];
